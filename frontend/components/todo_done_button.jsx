2b5a6d032e2e1d31464ff0bbfb471f7efc85a1db
App Academy Student <student@appacademy.io>
2016-04-19 16:22:49 -0700

    Todo Done Buttom component

---
+++ frontend/components/todo_done_button.jsx
@@ -0,0 +1,21 @@
+var React = require('react');
+var TodoStore = require('../stores/todo_store');
+
+var TodoDoneButton = React.createClass ({
+
+  handleToggle: function(event){
+    TodoStore.toggleDone(this.props.todo.id);
+  },
+
+  render: function() {
+    var content = this.props.todo.done ? "Undo" : "Done";
+
+    return (
+      <button onClick={this.handleToggle}>{content}</button>
+    );
+  }
+
+});
+
+
+module.exports = TodoDoneButton;
--- frontend/components/todo_list_item.jsx
+++ frontend/components/todo_list_item.jsx
@@ -1,21 +1,19 @@
 var React = require('react');
 var TodoDeleteButton = require('./todo_delete_button');
+var TodoDoneButton = require('./todo_done_button');
 
 var TodoListItem = React.createClass({
+  render: function(){
 
-render: function(){
-
-  return (
-      <div>
-        <div>{this.props.todo.title}</div>
-        <div>{this.props.todo.body}</div>
-        <TodoDeleteButton todoId={this.props.todo.id} />
-      </div>
-  );
-
-}
-
+    return (
+        <div>
+          <div>{this.props.todo.title}</div>
+          <div>{this.props.todo.body}</div>
+          <TodoDeleteButton todoId={this.props.todo.id} />
+          <TodoDoneButton todo={this.props.todo} />
+        </div>
+    );
+  }
 });
-
 
 module.exports = TodoListItem;
